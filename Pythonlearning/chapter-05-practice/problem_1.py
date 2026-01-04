7b19d693e299737a5cb56fe6b289bb3161375c81
sahilsaiyed-oss <sahilsaiyed067@gmail.com>
2026-01-04 20:48:55 +0530

    Add Chapter 5 practice set

---
+++ Pythonlearning/chapter-05-practice/problem_1.py
@@ -0,0 +1,11 @@
+a = {
+
+    "madad" : "help",
+    "kursi" : "chair",
+    "bili"  : "cat",
+    "kaam"  : "work"
+    }
+
+
+word = input(" enter the word which you are looking for ")
+print(a[word])
