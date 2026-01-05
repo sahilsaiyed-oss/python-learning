c2210adea3dfc9975e767d8d8b857df6778cbfc3
sahilsaiyed-oss <sahilsaiyed067@gmail.com>
2026-01-05 23:14:01 +0530

    Add Chapter 6  practice problems

---
+++ Pythonlearning/chapter-06-practice/problem_4.py
@@ -0,0 +1,6 @@
+username = input("enter username :")
+
+if(len(username)<10):
+    print("Your username contains less than 10 characters")
+else:
+    print("Your username contains more than 10 characters")    
