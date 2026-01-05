85960d545e67dd8656beabd6ac6e59b65aeb5897
sahilsaiyed-oss <sahilsaiyed067@gmail.com>
2026-01-05 22:21:32 +0530

    Add Chapter 6 and practice problems

---
+++ Pythonlearning/chapter-06/conditionals.py
@@ -0,0 +1,20 @@
+# If  else ladder
+
+a = int(input("Enter your age : "))
+
+
+if (a >= 18) : 
+    print("you are above the age of consent ")
+    print("Good for you ")
+
+else :
+    print("you are below the age of consent ")  
+
+print("End of programe")      
+
+
+
+
+
+
+
---
+++ Pythonlearning/chapter-06/if_elif_else_ladder.py
@@ -0,0 +1,22 @@
+# If elif else ladder
+
+a = int(input("Enter your age : "))
+
+
+if (a >= 18) : 
+    print("you are above the age of consent ")
+    print("Good for you ")
+
+elif (a < 0):
+    print("you are entering invalid age are you mentally stable or not ")
+
+elif (a == 0):
+    print("you are entering 0")
+else :
+    print("you are below the age of consent ")  
+
+print("End of programe")      
+
+
+
+
