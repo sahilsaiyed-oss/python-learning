b34562672b061f3f165d42c63ba8deb2b96c7d48
sahilsaiyed-oss <sahilsaiyed067@gmail.com>
2026-01-06 23:17:04 +0530

    Add chapter 7 - for loop iteration examples

---
+++ Pythonlearning/chapter-07/05_for_loop_iterate.py
@@ -0,0 +1,14 @@
+# for loop with lists
+l = [1,4,6,243,6,764]
+for i in l:
+    print(i)
+
+#for loop with tuples
+t = (6,231,75,122) 
+for i in t:
+    print(i)
+
+#for loop with strings
+s = "Harry" 
+for i in s: 
+    print(i)   
